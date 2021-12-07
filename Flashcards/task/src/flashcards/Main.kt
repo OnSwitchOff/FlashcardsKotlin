15dba9fb9203acf73eb932ada93ac1852c75a4ba
Viktor Kassov <kassov@mail.ua>
2021-12-07 14:32:19 +0200

    2/7+

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,16 +1,35 @@
 package flashcards
 
 fun main() {
-    Card("Yes?", "Yes!!!").print()
+    val card = Card()
+    card.checkAnswer(readLine()!!)
 }
 
-class Card(_front: String, _back: String) {
-    private val front = _front
-    private val back = _back
+class Card{
+    private val front: String
+    private val back: String
+    constructor() {
+        this.front = readLine()!!
+        this.back = readLine()!!
+    }
+    constructor(_front: String, _back: String){
+        this.front = _front
+        this.back = _back
+    }
+    fun checkAnswer(answer: String) {
+        println(if (answer.equals(back, ignoreCase = false)) "Your answer is right!" else "Your answer is wrong...")
+    }
     fun print() {
         println("Card:")
         println(front)
         println("Definition:")
         println(back)
+    }
+}
+
+class FlashCards() {
+    val cards: MutableList<Card> = mutableListOf()
+    fun addCard(card: Card) {
+        cards.add(card)
     }
 }
