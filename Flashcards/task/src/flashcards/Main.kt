a4953b426afa5a3880b5019589305ebfbc5dbfb5
Viktor Kassov <kassov@mail.ua>
2022-01-10 15:14:19 +0200

    5

--- Flashcards/task/src/flashcards/Main.kt
+++ Flashcards/task/src/flashcards/Main.kt
@@ -1,7 +1,91 @@
 package flashcards
 
+import java.io.File
+import kotlin.random.Random
+
 fun main() {
-    menu()
+    menu2()
+}
+
+fun menu2(){
+    val flashcards = FlashCards()
+    println("Input the action (add, remove, import, export, ask, exit):")
+    while (true)
+    {
+        when (readLine()!!) {
+            "add" -> addCommand(flashcards)
+            "remove" -> removeCommand(flashcards)
+            "import" -> importCommand(flashcards)
+            "export" -> exportCommand(flashcards)
+            "ask" -> askCommand(flashcards)
+            "exit" -> {
+                println("Bye bye!")
+                return
+            }
+        }
+    }
+
+}
+
+fun askCommand(flashcards: FlashCards) {
+    println("How many times to ask?")
+    val n = readLine()!!.toInt()
+    for (i in 0 until n){
+        val card: Card = flashcards.getRandomCard()
+        println("Print the definition of \"${card.front}\":")
+        val answer = readLine()!!
+        val filtred = flashcards.cards.filter { it.value.checkAnswer(answer) }
+        when {
+            card.back == answer -> println("Correct!")
+            filtred.isEmpty() -> println("Wrong. The right answer is \"${card.back}\"")
+            else -> filtred.forEach{ println("Wrong. The right answer is \"${card.back}\", but your definition is correct for \"${it.value.front}\".")}
+        }
+    }
+}
+
+fun exportCommand(flashcards: FlashCards) {
+    println("File name:")
+    val fileName = readLine()!!
+    val file = File(fileName)
+    flashcards.save(file)
+}
+
+fun importCommand(flashcards: FlashCards) {
+    println("File name:")
+    val fileName = readLine()!!
+    val file = File(fileName)
+    if (file.exists()){
+        flashcards.load(file)
+    } else {
+        println("File not found.")
+    }
+}
+
+fun removeCommand(flashcards: FlashCards) {
+    println("Which card?")
+    val front: String = readLine()!!
+    if (flashcards.checkForExistTerm(front)) {
+        flashcards.removeCard(front)
+        println("The card has been removed.")
+    } else {
+        println("Can't remove \"$front\": there is no such card.")
+    }
+}
+
+fun addCommand(flashcards: FlashCards) {
+    println("The card:")
+    val front: String = readLine()!!
+    if (flashcards.checkForExistTerm(front)) {
+        println("The card \"$front\" already exists.")
+    } else {
+        println("The definition of the card:")
+        val back: String = readLine()!!
+        if (flashcards.checkForExistDefinition(back)){
+            println("The definition \"$back\" already exists")
+        } else {
+            flashcards.addCard(Card(front, back))
+        }
+    }
 }
 
 fun menu(){
@@ -73,5 +157,31 @@ class FlashCards() {
             return true
         }
         return false
+    }
+
+    fun removeCard(front: String) {
+        cards.remove(cards.filterValues{ it.front == front }.keys.first())
+    }
+
+    fun load(file: File) {
+        cards.clear()
+        val lines = file.readLines()
+        for (i in 0 until lines.lastIndex step 2) {
+            addCard(Card(lines[i],lines[i + 1]))
+        }
+        println("${cards.size} cards have been loaded.")
+    }
+
+    fun save(file: File) {
+        file.createNewFile()
+        cards.forEach{
+            file.appendText(it.value.front + "\n")
+            file.appendText(it.value.back + "\n")
+        }
+    }
+
+    fun getRandomCard(): Card {
+        val k: Int = Random.nextInt(cards.size)
+        return cards[k]!!
     }
 }
